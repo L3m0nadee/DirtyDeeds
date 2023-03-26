fe26200d509a6c9341b1044528443db806ecba47
L3m0nadee <107894813+L3m0nadee@users.noreply.github.com>
2023-03-26 13:51:43 +1300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function openPopup() {
+    document.getElementById("popup").classList.toggle("open");
+}
+
+
+function openOverlay() {
+    document.getElementById("overlay").classList.toggle("open");
+}
+
+
+function openCover() {
+    document.getElementById("cover").classList.toggle("open")
+}
+
+
+function openMask() {
+    document.getElementById("mask").classList.toggle("open")
+}
+
+
+
+
+
